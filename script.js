9e342ed03fa80108d03812c3c238043968eb2b94
Michael Salisbury <m.slzbry@gmail.com>
2023-06-02 11:28:29 -0700

    let const again

--- script.js
+++ script.js
@@ -118,12 +118,12 @@ function generatePassword () {
 }
 
 // Get references to the #generate element
-var generateBtn = document.querySelector('#generate')
+const generateBtn = document.querySelector('#generate')
 
 // Write password to the #password input
 function writePassword () {
-  var password = generatePassword()
-  var passwordText = document.querySelector('#password')
+  let password = generatePassword()
+  const passwordText = document.querySelector('#password')
 
   passwordText.value = password
 }
